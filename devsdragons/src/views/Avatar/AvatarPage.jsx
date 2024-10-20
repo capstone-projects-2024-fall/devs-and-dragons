e3457807fc1a940e055da48d63cdb455df2f8f35
bhoiwalapreet <tun38627@temple.edu>
2024-10-19 20:17:35 -0400

    changed background color

--- devsdragons/src/views/Avatar/AvatarPage.jsx
+++ devsdragons/src/views/Avatar/AvatarPage.jsx
@@ -5,10 +5,10 @@ import triangleImg from './triangle.png';
 
 
 const AvatarPage = () => (
-    <div>
+    <div style={{ backgroundColor: 'pink', height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
         <h1> Avatar Page </h1>
         {/*<HUD />*/}
-        <div>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             <img src={squareImg} alt ="Square Shape" style = {{ width:'100px'}} />
             <img src={circleImg} alt ="Circle Shape" style = {{ width:'100px', marginLeft: '10px'}} />
             <img src={triangleImg} alt ="Triangle Shape" style = {{ width:'100px', marginLeft: '10px'}} />
