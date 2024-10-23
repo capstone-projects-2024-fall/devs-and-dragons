b9eef2b14113a9d13dc5e24b353ad7415f95579f
bhoiwalapreet <tun38627@temple.edu>
2024-10-23 14:49:52 -0400

    added dropdown for triangle shape

--- devsdragons/src/views/Avatar/AvatarPage.jsx
+++ devsdragons/src/views/Avatar/AvatarPage.jsx
@@ -3,19 +3,34 @@ import React, { useState } from 'react';
 import squareBlue from './squareBlue.png';
 import squareGreen from './squareGreen.png';
 import circleImg from './circle.png';
-import triangleImg from './triangle.png';
+import triangleBlue from './triangleBlue.png';
+import triangleGreen from './triangleGreen.png';
 
 
 const AvatarPage = () => {
     const [squareImage, setSquareImage] = useState(squareBlue);
+    const [triangleImage, setTriangleImage] = useState(triangleBlue);
+
+    //handling square change 
     const handleSquareChange = (event) => {
         //changes the image based on user's selection 
-        const selectedColor = event.target.value;
-        if (selectedColor === 'blue') {
+        const selectedSquareColor = event.target.value;
+        if (selectedSquareColor === 'blue') {
             setSquareImage(squareBlue);
-        } else if (selectedColor === 'green') {
+        } else if (selectedSquareColor === 'green') {
             setSquareImage(squareGreen);
         }
+    }
+
+    //handling triangle change
+    const handleTriangleChange = (event) => {
+         //changes the image based on user's selection 
+         const selectedTriangleColor = event.target.value;
+         if(selectedTriangleColor === 'blue') {
+            setTriangleImage(triangleBlue);
+         } else if (selectedTriangleColor === 'green') {
+            setTriangleImage(triangleGreen);
+         }
     }
 
     return (
@@ -23,10 +38,11 @@ const AvatarPage = () => {
             <h1> Avatar Page </h1>
             {/*<HUD />*/}
 
-            {/*adding drop down to change color of shape*/}
+           
             {/* wrapping all shapes in flex container to make sure they align next to each other*/}
             <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
 
+                {/* square and it's dropdown*/}
                 <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginRight: '10px' }}>
                     <select onChange={handleSquareChange}> {/*triggers the handleSquareChange function so user can select option*/}
                         <option value="blue"> Blue Square</option>
@@ -35,11 +51,17 @@ const AvatarPage = () => {
                     <img src={squareImage} alt="Square Shape" style={{ width: '100px' }} />
 
                 </div>
+                 {/* triangle and it's dropdown*/}
+                <div style={{ display: 'flex', flexDirection: 'column',alignItems: 'center', marginLeft:'10px' }}>
+                    <select onChange= {handleTriangleChange}>
+                        <option value="blue"> Blue Triangle</option>
+                        <option value="green"> Green Triangle</option>
+                    </select>
+                    <img src={triangleImage} alt="Triangle Shape" style={{ width: '100px', marginLeft: '10px' }} />
+                    {/* <img src={circleImg} alt="Circle Shape" style={{ width: '100px', marginLeft: '10px' }} /> */}
 
-                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                    <img src={circleImg} alt="Circle Shape" style={{ width: '100px', marginLeft: '10px' }} />
-                    <img src={triangleImg} alt="Triangle Shape" style={{ width: '100px', marginLeft: '10px' }} />
                 </div>
+                <img src={circleImg} alt="Circle Shape" style={{ width: '100px', marginLeft: '10px' }} />
             </div>
         </div>
     )
