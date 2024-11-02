f8aa8b4e632d582097659f75d966bd5f7e61dfa4
Animish Tenneti <justanimish@proton.me>
2024-11-02 14:27:02 -0700

    setting canvas and context further

--- devsdragons/src/components/QuestCanvas/QuestCanvas.jsx
+++ devsdragons/src/components/QuestCanvas/QuestCanvas.jsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react"
+
 const QuestCanvas = () => {
 
-  //const canvasRef = useRef(null)
+  const canvasRef = useRef(null)
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+  })
 
   return (
     <div>QuestCanvas</div>
