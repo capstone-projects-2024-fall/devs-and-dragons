7c8ac8dd2591937cf1ea7592374c46ff41641426
Animish Tenneti <justanimish@proton.me>
2024-11-02 11:48:19 -0400

    previewing basic quest canvas page in quest game view

--- devsdragons/src/views/QuestGameView/QuestGameView.jsx
+++ devsdragons/src/views/QuestGameView/QuestGameView.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import QuestCanvas from "../../components/QuestCanvas/QuestCanvas"
 
 const QuestGameView = () => {
   return (
-    <div>QuestGameView</div>
+    <div>
+        <QuestCanvas />
+    </div>
   )
 }
 
