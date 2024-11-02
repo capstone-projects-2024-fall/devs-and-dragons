83255c2decd02b8782c10045f288c9e625f050f3
Animish Tenneti <justanimish@proton.me>
2024-11-02 14:34:29 -0700

    initialized the mechanism to set background as a variable with UseState hook

--- devsdragons/src/components/QuestCanvas/QuestCanvas.jsx
+++ devsdragons/src/components/QuestCanvas/QuestCanvas.jsx
@@ -1,13 +1,23 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 const QuestCanvas = () => {
 
-  const canvasRef = useRef(null)
+  const canvasRef = useRef(null);
+  const [background, setBackground] = useState(new Image())
 
-  useEffect(() => {
+  useEffect( () => {
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
-  })
+
+      // Render background
+    const render = () => {
+      // Previous canvas's context should be cleared and new canvas should be loaded in each time
+      context.clear
+    }
+  
+  } )
+
+  
 
   return (
     <div>QuestCanvas</div>
