7d9120272a44d21a22b7b9391fcfc7b06e8493a2
Animish Tenneti <justanimish@proton.me>
2024-11-03 07:52:14 -0800

    deleted component and put everything into view

--- devsdragons/src/views/QuestGameView/QuestView.jsx
+++ devsdragons/src/views/QuestGameView/QuestView.jsx
@@ -1,13 +1,73 @@
-import QuestCanvas from "../../components/QuestCanvas/QuestCanvas"
-import HUD from "../../components/HUD/HUD"
+import { useEffect, useState, useRef } from "react";
+import backgroundImgSrc from "../../assets/black-screen.jpg"; // Cast the image as a variable to be used
 
 const QuestView = () => {
+  // Canvas reference
+  const canvasRef = useRef(null);
+
+  // Background reference
+  const [background, setBackground] = useState(new Image());
+
+  useEffect(() => {
+    const backgroundImg = new Image(); // Create a new Image object
+    backgroundImg.src = backgroundImgSrc; // Load the image
+
+    // Ensure background is loaded
+    backgroundImg.onload = () => {
+      setBackground(backgroundImg); // Set the loaded image as background
+      resizeCanvas(); // function to dynamically resize wallpaper to changed window size
+    };
+
+    const render = () => {
+      const canvas = canvasRef.current;
+      const context = canvas.getContext("2d");
+
+      // Clear the previous canvas
+      context.clearRect(0, 0, canvas.width, canvas.height);
+
+      // Draw background image only when it's loaded
+      if (background.src) {
+        context.drawImage(background, 0, 0, canvas.width, canvas.height);
+      }
+
+      requestAnimationFrame(render);
+    };
+
+    render();
+
+    const handleResize = () => {
+      resizeCanvas();
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize); // remove when game is exited
+      cancelAnimationFrame(render);
+    };
+  }, [background]);
+
+  const resizeCanvas = () => {
+    const canvas = canvasRef.current;
+
+    // if canvas exists
+    if (canvas) {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+
+      // Update context and background
+      const context = canvas.getContext("2d");
+      if (background.src) {
+        context.drawImage(background, 0, 0, canvas.width, canvas.height);
+      }
+    }
+  };
+
   return (
     <div className="my-games-page">
-        <HUD />
-        <QuestCanvas />
+      <canvas ref={canvasRef} style={{ border: "1px solid #000" }} />
     </div>
-  )
-}
+  );
+};
 
-export default QuestView
+export default QuestView;
