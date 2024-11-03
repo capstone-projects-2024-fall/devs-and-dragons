6116d1ded4b39a6a5cd8316cfbaefc9be14eb518
Animish Tenneti <justanimish@proton.me>
2024-11-02 17:47:01 -0700

    clarified return

--- devsdragons/src/components/GameComponents/Wallpaper.jsx
+++ devsdragons/src/components/GameComponents/Wallpaper.jsx
@@ -1,9 +1,7 @@
 import { useEffect } from "react";
 
 const Wallpaper = ({ canvasRef, backgroundSrc }) => {
-  return (
-    <div>Wallpaper</div>
-  )
+  return null; // Wallpaper is to be used by a higher level component, it doesn't return anything
 }
 
 export default Wallpaper
