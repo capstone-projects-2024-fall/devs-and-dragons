b361eb0384442d57d5e93cd9810e0e2e0100df3b
Animish Tenneti <justanimish@proton.me>
2024-11-03 08:03:12 -0800

    created signature line specifying which arguments resizeCanvas helper function needs

---
+++ devsdragons/src/components/QuestViewHelpers/reseizeCanvas.js
@@ -0,0 +1,6 @@
+// If browser window dimensions are modified, resize the quest view as needed
+
+export const resizeCanvas = (canvasRef, background) => {
+    const canvas = canvasRef.current
+    
+}
