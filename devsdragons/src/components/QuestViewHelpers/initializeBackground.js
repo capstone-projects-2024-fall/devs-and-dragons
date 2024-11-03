0976730c83ca794f5642065fbe97b8179b431826
Animish Tenneti <justanimish@proton.me>
2024-11-03 08:01:16 -0800

    completed helper function, sets image and loades and calls upon resizing

--- devsdragons/src/components/QuestViewHelpers/initializeBackground.js
+++ devsdragons/src/components/QuestViewHelpers/initializeBackground.js
@@ -1,5 +1,16 @@
 // With an image, intialize the background for the first time
 
-export const intializeBackground = (src, setBackground, resizeCanvas, canvasRef) => {
-    const backgroundImg = new Image()
-}
+export const intializeBackground = (
+  src,
+  setBackground,
+  resizeCanvas,
+  canvasRef
+) => {
+  const backgroundImg = new Image();
+  backgroundImg.src = src;
+
+  backgroundImg.onload = () => {
+    setBackground(backgroundImg);
+    resizeCanvas(canvasRef, backgroundImg);
+  };
+};
