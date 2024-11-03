ad91d31fcf02f1ac90df7739e4deab61289311ba
Animish Tenneti <justanimish@proton.me>
2024-11-03 07:59:33 -0800

    specified signature line of function with which parameters to be used and are required

---
+++ devsdragons/src/components/QuestViewHelpers/initializeBackground.js
@@ -0,0 +1,5 @@
+// With an image, intialize the background for the first time
+
+export const intializeBackground = (src, setBackground, resizeCanvas, canvasRef) => {
+    const backgroundImg = new Image()
+}
