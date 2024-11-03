8ec7c37231df8c2e259208144c0d8ad52370afae
Animish Tenneti <justanimish@proton.me>
2024-11-03 08:10:24 -0800

    completed function

--- devsdragons/src/components/QuestViewHelpers/drawBackground.js
+++ devsdragons/src/components/QuestViewHelpers/drawBackground.js
@@ -1,1 +1,10 @@
-export const first = (second) => {third}
+// Draw the background on the given canvas with the given background
+
+export const drawBackground = (canvasRef, background) => {
+  const canvas = canvasRef.current;
+
+  if (canvas && background.src) {
+    const context = canvas.getContext("2d");
+    context.drawImage(background, 0, 0, canvas.width, canvas.height);
+  }
+};
