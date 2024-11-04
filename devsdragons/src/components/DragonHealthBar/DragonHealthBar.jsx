0fbc5509a6953fdcc30f05931f4b54c7bc82cdf6
bhoiwalapreet <tun38627@temple.edu>
2024-11-04 17:49:54 -0500

    Created Dragon's Health Bar and Tested

--- devsdragons/src/components/DragonHealthBar/DragonHealthBar.jsx
+++ devsdragons/src/components/DragonHealthBar/DragonHealthBar.jsx
@@ -3,13 +3,13 @@ import React, {useState} from 'react';
 
 
 
-const PlayerHealthBar = () => {
-    const[health, setHealth] = useState(100); //random value for now
+const DragonHealthBar = () => {
+    const[health, setHealth] = useState(150); //random value for now (more than player)
     //health -> variable holding current health
     //setHealth -> function to update health 
 
 
-    //function to decrease player's health
+    //function to decsease player's health
     const decreaseHealth = () => {
         setHealth((prevHealth) => Math.max(prevHealth - 10, 0)); 
     };
@@ -17,17 +17,17 @@ const PlayerHealthBar = () => {
     return ( 
         <div>
              {/* Button to decrease health */}
-            <button style = {{backgroundColor: 'red', color: 'white'}} onClick= {decreaseHealth}>TakeDamage(Player)</button>
+            <button style = {{backgroundColor: 'red', color: 'white'}} onClick= {decreaseHealth}>TakeDamage(Dragon)</button>
              {/* Display current health value */}
             <p>Health: {health}</p> {/*states health*/}
 
             {/*healthbar*/}
             <div style = {{width: '200px', height: '20px', backgroundColor: '#4a4a4a', position: 'relative' }}> {/*empty (outer) bar is grey*/}
-            <div style = {{width:`${health}%`, height: '100%', backgroundColor: '#b3e024'}} ></div> {/* filled (inner) bar is bright green*/}
+            <div style = {{width:`${(health / 150) * 100}%`, height: '100%', backgroundColor: '#b3e024'}} ></div> {/* filled (inner) bar is bright green*/}
             </div>
         </div>
     );
 }
 
 
-export default PlayerHealthBar;
+export default DragonHealthBar;
