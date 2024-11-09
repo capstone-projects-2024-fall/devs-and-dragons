f2c96d8621caf23ab112ccadc9d40e5466b1536c
Animish Tenneti <justanimish@proton.me>
2024-11-09 18:29:57 -0500

    changed wallpaper

--- devsdragons/src/views/QuestGameView/QuestView.jsx
+++ devsdragons/src/views/QuestGameView/QuestView.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, useRef } from "react";
-import backgroundImgSrc from "../../assets/black-screen.jpg"; // Cast the image as a variable to be used
+import backgroundImgSrc from "../../assets/images.jpg"; // Cast the image as a variable to be used
 import { loadBackdropFunction } from "../../components/QuestViewHelpers/Functions/loadBackdropFunction";
 import { drawBackgroundFunction } from "../../components/QuestViewHelpers/Functions/drawBackgroundFunction";
 import { resizeCanvasFunction } from "../../components/QuestViewHelpers/Functions/resizeCanvasFunction";
