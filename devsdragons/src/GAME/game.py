e185ba93b8a1b7adebc6291c4765bdbaebe859e7
Animish Tenneti <justanimish@proton.me>
2024-11-10 10:14:38 -0500

    Deleted separate fighter class as it's better to have everything in 1 file for now, Instantiated a knight (fighter)

--- devsdragons/src/GAME/game.py
+++ devsdragons/src/GAME/game.py
@@ -23,6 +23,29 @@ background_image = pygame.image.load('images.jpg').convert_alpha()
 def draw_background():
     screen.blit(background_image, (0, 0))
 
+class Fighter:
+    def __init__(self, x, y, name, max_hp, strength, potions):
+        self.name = name
+        self.max_hp = max_hp
+        self.hp = max_hp
+        self.strength = strength
+        self.start_potions = potions
+        self.potions = potions
+        self.alive = True
+        self.animation_list = []
+        self.frame_index = 0
+        # 0 indicating idle, 1 attack, 2 hurt, 3 dead
+        self.action = 0
+        self.update_time = pygame.time.get_ticks()
+        self.image = pygame.image.load(f'img/{self.name}/Idle/0.png')
+        self.rect = self.image.get_rect()
+        self.rect_center = (x, y)
+
+    def draw(self):
+        screen.blit(self.image, self.rect)
+
+knight = Fighter(200, 260, 'Knight', 30, 10, 3)
+
 run = True
 while run:
     clock.tick(fps)
