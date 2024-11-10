44d7c62279a79d3b5cba9f1cc124127b10595d68
Animish Tenneti <justanimish@proton.me>
2024-11-09 19:35:32 -0500

    backrgound img

--- devsdragons/src/GAME/game.py
+++ devsdragons/src/GAME/game.py
@@ -14,3 +14,12 @@ screen_width = 800
 screen_height = 400 + bottom_panel
 
 screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Battle")
+
+# Load Images
+# Background image
+background_image = pygame.image.load('images.jpg').convert_alpha()
+
+run = True
+while run:
+    clock.tick(fps)
