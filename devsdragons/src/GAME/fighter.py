21afd28740d6253a0bb345ba7a812f19296c5d0c
Animish Tenneti <justanimish@proton.me>
2024-11-09 20:03:24 -0500

    created fighter.py file which is our player

---
+++ devsdragons/src/GAME/fighter.py
@@ -0,0 +1,6 @@
+class Fighter():
+    def __init__(self, x, y, name, max_hp, strength, potions):
+        self.name = name
+        self.max_hp = max_hp
+        self.hp = max_hp
+        self.strength = strength
