5cc77b5a87286b39d9536068ff466da2f19b3da0
Animish Tenneti <justanimish@proton.me>
2024-11-10 14:29:38 -0500

    Created health bar class

--- devsdragons/src/GAME/game.py
+++ devsdragons/src/GAME/game.py
@@ -37,6 +37,13 @@ background_image = pygame.transform.scale(background_image, (screen_width, scree
 def draw_background():
     screen.blit(background_image, (0, 0))
 
+class HealthBar:
+    def __init__(self, x, y, hp, max_hp):
+        self.x = x
+        self.y = y
+        self.hp = hp
+        self.max_hp = max_hp
+
 class Fighter:
     def __init__(self, x, y, name, max_hp, strength, potions):
         self.name = name
