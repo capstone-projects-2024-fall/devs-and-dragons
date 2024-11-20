ab5f2ae2723603b4d7a2fb16043e2d7da9cfd311
Jericho R Orienza <93217801+Jericho-R-Orienza@users.noreply.github.com>
2024-11-20 16:10:53 -0500

    remove commented out lines

--- devsdragons/src/App.jsx
+++ devsdragons/src/App.jsx
@@ -1,53 +1,53 @@
-// import LoginPage from './views/LoginPage/LoginPage.jsx';
-// import AvatarPage from './views/Avatar/AvatarPage.jsx';
-// import CreateQuestsPage from './views/QuestPage/CreateQuests.jsx';
-// import MyQuestsPage from './views/QuestPage/MyQuests.jsx';
-// import HomePage from './views/HomePage/HomePage.jsx';
-// import ProtoTypePage from './views/ProtoTypePage/ProtoTypePage.jsx';
-// import CodeEditor from './views/Editor/CodeEditor.jsx';
-// import { DropDown, Button, Select, HUD } from './views/Examples'; // example views to showcase each component
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './views/LoginPage/LoginPage.jsx';
+import AvatarPage from './views/Avatar/AvatarPage.jsx';
+import CreateQuestsPage from './views/QuestPage/CreateQuests.jsx';
+import MyQuestsPage from './views/QuestPage/MyQuests.jsx';
+import HomePage from './views/HomePage/HomePage.jsx';
+import ProtoTypePage from './views/ProtoTypePage/ProtoTypePage.jsx';
+import CodeEditor from './views/Editor/CodeEditor.jsx';
+import { DropDown, Button, Select, HUD } from './views/Examples'; // example views to showcase each component
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React, {useState} from 'react';
-// import theme from './theme/theme.js';
-// import { ThemeProvider, CssBaseline } from '@mui/material';
-// import {QuestProvider} from './views/QuestPage/QuestContext.jsx'
-// import QuestMainPage from './views/QuestPage/QuestMainPage.jsx';
+import theme from './theme/theme.js';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import {QuestProvider} from './views/QuestPage/QuestContext.jsx'
+import QuestMainPage from './views/QuestPage/QuestMainPage.jsx';
 import PlayerAnimation from '../GAME/Player/player.jsx';
 
 
 
 function App() {
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  // const handleLogin = (loginStatus) => {
-  //   setIsLoggedIn(loginStatus);
-  // };
+  const handleLogin = (loginStatus) => {
+    setIsLoggedIn(loginStatus);
+  };
 
-  // const handleLogout = () => {
-  //   setIsLoggedIn(false);
-  // };
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
 
   return (
-    // <ThemeProvider theme={theme}>
-    //   <CssBaseline />
-    //   <Router>
-    //     <QuestProvider>
-    //       <Routes>
-    //         <Route path="/" element={<LoginPage />} />
-    //         <Route path="/code-editor" element={<CodeEditor />} />  
-    //         <Route path="/home-page" element={<HomePage />} />
-    //         <Route path="/create-quests" element={<CreateQuestsPage />}/>
-    //         <Route path="/my-quests" element={<MyQuestsPage/>} />
-    //         <Route path="/quest-main" element={<QuestMainPage />} /> 
-    //         <Route path="/guilds" element={<GuildsPage />} />
-    //         <Route path="/avatar" element={<AvatarPage />} />
-    //       </Routes>
-    //     </QuestProvider>
-    //   </Router>
-    // </ThemeProvider>
-    <div>
-      <PlayerAnimation />
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <QuestProvider>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/code-editor" element={<CodeEditor />} />  
+            <Route path="/home-page" element={<HomePage />} />
+            <Route path="/create-quests" element={<CreateQuestsPage />}/>
+            <Route path="/my-quests" element={<MyQuestsPage/>} />
+            <Route path="/quest-main" element={<QuestMainPage />} /> 
+            <Route path="/guilds" element={<GuildsPage />} />
+            <Route path="/avatar" element={<AvatarPage />} />
+          </Routes>
+        </QuestProvider>
+      </Router>
+    </ThemeProvider>
+    // <div>
+    //   <PlayerAnimation />
+    // </div>
   );
 }
 
