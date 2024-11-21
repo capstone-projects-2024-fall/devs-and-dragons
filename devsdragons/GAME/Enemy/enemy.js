a916f05cea89f0983f3ce1ea9a42058c989288ec
Jericho R Orienza <93217801+Jericho-R-Orienza@users.noreply.github.com>
2024-11-21 10:54:26 -0500

    Add button functionaliyu - can now switch animations to demo

--- devsdragons/GAME/Enemy/enemy.js
+++ devsdragons/GAME/Enemy/enemy.js
@@ -18,7 +18,6 @@ export default function initDragonAnimation() {
             this.y = (canvasHeight - this.height) / 2;
             this.tickCount = 0;
             this.ticksPerFrame = 10;
-            this.frameIndex = 0;
             this.changeAnimation('dragonIdle', 3); // Set defauly animation to idle
         }
 
@@ -47,6 +46,16 @@ export default function initDragonAnimation() {
     }
 
     const dragon = new Dragon(canvas.width, canvas.height, "dragonIdle", 3);
+
+    document.getElementById("attack").addEventListener("click", () => {
+        dragon.changeAnimation('dragonAttack', 15);
+    });
+    document.getElementById("hurt").addEventListener("click", () => {
+        dragon.changeAnimation('dragonHurt', 3);
+    });
+    document.getElementById("death").addEventListener("click", () => {
+        dragon.changeAnimation('dragonDeath', 4);
+    });
 
     function animate() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
