b3e2ea8077e93788937b6025485d9042ee9866c2
Jericho-R-Orienza <jorienza2019@gmail.com>
2024-12-04 20:56:59 -0500

    remove comments, add adjust y to change player based on background

--- devsdragons/src/views/QuestPage/gamePlayer.js
+++ devsdragons/src/views/QuestPage/gamePlayer.js
@@ -1,67 +1,4 @@
-// export default function initGamePlayerAnimation() {
-//     const canvas = document.getElementById("playerCanvas");
-//     if (!canvas) return; // Ensure canvas exists
-//     const ctx = canvas.getContext("2d");
-//     canvas.width = 500;
-//     canvas.height = 500;
-
-//     class Knight {
-//         constructor(canvasWidth, canvasHeight, imageID, max) {
-//             this.canvasWidth = canvasWidth;
-//             this.canvasHeight = canvasHeight;
-//             this.image = document.getElementById(imageID);
-//             this.scale = 4;
-//             this.spriteWidth = 64;
-//             this.spriteHeight = 64;
-//             this.width = this.spriteWidth * this.scale;
-//             this.height = this.spriteHeight * this.scale;
-//             this.x = (canvasWidth - this.width) / 2;
-//             this.y = (canvasHeight - this.height);
-//             this.tickCount = 0;
-//             this.ticksPerFrame = 10;
-//             this.maxFrame = max;
-//             this.frameIndex = 0;
-//             this.playOnce = false;
-//             this.changeAnimation('playerIdle', 7);
-//         }
-
-//         changeAnimation(state, frames) {
-//             this.image = document.getElementById(state);
-//             this.maxFrame = frames;
-//             this.frameIndex = 0;
-//             this.playOnce = state !== 'playerIdle';
-//         }
-
-//         draw(context) {
-//             let sx = 16 + this.frameIndex * 96;
-//             let sy = 16;
-//             context.drawImage(this.image, sx, sy, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
-//         }
-
-//         update() {
-//             this.tickCount++;
-//             if (this.tickCount > this.ticksPerFrame) {
-//                 this.tickCount = 0;
-//                 if (this.frameIndex >= this.maxFrame - 1 && this.playOnce) {
-//                     this.changeAnimation('playerIdle', 7);
-//                 } else {
-//                     this.frameIndex = (this.frameIndex + 1) % this.maxFrame;
-//                 }
-//             }
-//         }
-//     }
-
-//     const knight = new Knight(canvas.width, canvas.height, "playerIdle", 7);
-//     function animate() {
-//         ctx.clearRect(0, 0, canvas.width, canvas.height);
-//         knight.draw(ctx);
-//         knight.update();
-//         requestAnimationFrame(animate);
-//     }
-//     animate();
-// }
-
-export default function initGamePlayerAnimation() {
+export default function initGamePlayerAnimation(adjust_y) {
     const canvas = document.getElementById("playerCanvas");
     if (!canvas) return null; // Return null if canvas is not found
     const ctx = canvas.getContext("2d");
@@ -69,7 +6,7 @@ export default function initGamePlayerAnimation() {
     canvas.height = 500;
 
     class Knight {
-        constructor(canvasWidth, canvasHeight, imageID, max) {
+        constructor(canvasWidth, canvasHeight, imageID, max, adjust_y) {
             this.canvasWidth = canvasWidth;
             this.canvasHeight = canvasHeight;
             this.image = document.getElementById(imageID);
@@ -79,9 +16,9 @@ export default function initGamePlayerAnimation() {
             this.width = this.spriteWidth * this.scale;
             this.height = this.spriteHeight * this.scale;
             this.x = (canvasWidth - this.width) / 2;
-            this.y = canvasHeight - this.height;
+            this.y = canvasHeight - this.height + adjust_y;
             this.tickCount = 0;
-            this.ticksPerFrame = 10;
+            this.ticksPerFrame = 5;
             this.maxFrame = max;
             this.frameIndex = 0;
             this.playOnce = false;
@@ -114,7 +51,7 @@ export default function initGamePlayerAnimation() {
         }
     }
 
-    const knight = new Knight(canvas.width, canvas.height, "playerIdle", 7);
+    const knight = new Knight(canvas.width, canvas.height, "playerIdle", 7, adjust_y);
     function animate() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         knight.draw(ctx);
